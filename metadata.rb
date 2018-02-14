4cc3de6b66af69df52714df5670b700988ae863d
Alex Denvir <alexdenvir@protecinnovations.co.uk>
2018-02-14 16:25:13 +0000

    Also install php7.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "dev@protecinnovations.co.uk"
 license          "All rights reserved"
 description      "Installs/Configures php7"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.3.0"
 
 %w{ ubuntu }.each do |os|
     supports os
--- recipes/php-fpm.rb
+++ recipes/php-fpm.rb
@@ -1,7 +1,7 @@
 include_recipe 'php7::ppa-ondrej'
 
 package "php7.1-fpm" do
-    action :upgrade
+  action :upgrade
 end
 
 template "/etc/php/7.1/fpm/conf.d/10-error_reporting.ini" do
@@ -19,6 +19,31 @@ template "/etc/php/7.1/fpm/conf.d/10-file_uploads.ini" do
 end
 
 template "/etc/php/7.1/fpm/conf.d/10-resource_limits.ini" do
+  source "resource_limits.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+package "php7.2-fpm" do
+  action :upgrade
+end
+
+template "/etc/php/7.2/fpm/conf.d/10-error_reporting.ini" do
+  source "error_reporting.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/etc/php/7.2/fpm/conf.d/10-file_uploads.ini" do
+  source "file_uploads.ini.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+end
+
+template "/etc/php/7.2/fpm/conf.d/10-resource_limits.ini" do
   source "resource_limits.ini.erb"
   owner "root"
   group "root"
